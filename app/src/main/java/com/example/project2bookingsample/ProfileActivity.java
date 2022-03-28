a51d116fdf09f95c256d6c5cbfe72d612acd165d
Yuxing Zhou <tomzhouyuxing@gmail.com>
2022-03-27 20:07:56 -0700

    more polished profile activitiy:

--- app/src/main/java/com/example/project2bookingsample/ProfileActivity.java
+++ app/src/main/java/com/example/project2bookingsample/ProfileActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,11 +14,26 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        // TODO: complete this section once the backend is ready.
+        String firstName = "Yuxing";
+        String lastName = "Zhou";
+        String netID = "9953222222";
+        Integer pastAppointments = 23;
+
+        TextView fullNameTextView = (TextView) findViewById(R.id.fullName);
+        fullNameTextView.setText("Name: "+firstName+" "+lastName);
+
+        TextView netIDView = (TextView) findViewById(R.id.netID);
+        netIDView.setText("NetID: "+netID);
+
+        TextView completedAppointments = (TextView) findViewById(R.id.completedAppointments);
+        completedAppointments.setText("You have completed "+pastAppointments+" appointments over the last year. Fight On!");
+
     }
     public void OnClickBackToHome(View view){
         Intent intent = new Intent(this, BookingActivity.class);
         // always passing the username as a means of user authentication
-        intent.putExtra("username", "9966389234");
+        intent.putExtra("netID", "9966389234");
         startActivity(intent);
     }
 }
