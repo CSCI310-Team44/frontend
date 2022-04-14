4c2eaeb617752b46b54ecafe01f454fabb9c1a62
Gary Mao <yunhanm@usc.edu>
2022-04-13 22:57:35 -0700

    commit

--- app/src/androidTest/java/com/example/project2bookingsample/ui/googleMap/GoogleMapTest.java
+++ app/src/androidTest/java/com/example/project2bookingsample/ui/googleMap/GoogleMapTest.java
@@ -1,8 +1,7 @@
-package com.example.project2bookingsample.ui.login;
+package com.example.project2bookingsample.ui.googleMap;
 
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
@@ -11,8 +10,13 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.allOf;
 
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -23,6 +27,7 @@ import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.example.project2bookingsample.R;
+import com.example.project2bookingsample.ui.login.LoginActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -31,34 +36,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LoginProfileTest {
+public class GoogleMapTest {
 
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
     @Test
-    public void loginActivityTest() {
+    public void googleMapTest() throws UiObjectNotFoundException, InterruptedException {
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.username),
                         childAtPosition(
@@ -92,26 +79,37 @@ public class LoginProfileTest {
                         isDisplayed()));
         appCompatEditText3.perform(pressImeActionButton());
 
-        ViewInteraction button = onView(
-                allOf(withId(R.id.button2), withText("View Profile"),
-                        childAtPosition(
-                                allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                3),
-                        isDisplayed()));
-        button.perform(click());
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.netID), withText("NetID: 1000000001"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView.check(matches(withText("NetID: 1000000001")));
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        UiObject marker = device.findObject(new UiSelector().descriptionContains("Village Fitness Center"));
+        marker.click();
 
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.fullName), withText("Name: mark redekopp"),
-                        withParent(withParent(withId(android.R.id.content))),
+        Thread.sleep(1000);
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.selectedCenter), withText("Village Fitness Center"),
+                        withParent(allOf(withId(R.id.constraintLayout),
+                                withParent(withId(android.R.id.content)))),
                         isDisplayed()));
-        textView2.check(matches(withText("Name: mark redekopp")));
+        textView.check(matches(withText("Village Fitness Center")));
+    }
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
--- app/src/androidTest/java/com/example/project2bookingsample/ui/googleMap/GoogleMapTest2.java
+++ app/src/androidTest/java/com/example/project2bookingsample/ui/googleMap/GoogleMapTest2.java
@@ -1,4 +1,4 @@
-package com.example.project2bookingsample.ui.login;
+package com.example.project2bookingsample.ui.googleMap;
 
 
 import static androidx.test.espresso.Espresso.onView;
@@ -11,8 +11,13 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.allOf;
 
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -22,43 +27,28 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import com.example.project2bookingsample.BookingActivity;
 import com.example.project2bookingsample.R;
+import com.example.project2bookingsample.ui.login.LoginActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LoginProfileTest {
+public class GoogleMapTest2 {
 
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
+    @Rule
+    public ActivityTestRule<BookingActivity> mBookingTestRule = new ActivityTestRule<>(BookingActivity.class);
     @Test
-    public void loginActivityTest() {
+    public void googleMapTest2() throws InterruptedException, UiObjectNotFoundException {
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.username),
                         childAtPosition(
@@ -92,26 +82,43 @@ public class LoginProfileTest {
                         isDisplayed()));
         appCompatEditText3.perform(pressImeActionButton());
 
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        UiObject marker = device.findObject(new UiSelector().descriptionContains("Village Fitness Center"));
+        marker.click();
+
+        Thread.sleep(1000);
+
         ViewInteraction button = onView(
-                allOf(withId(R.id.button2), withText("View Profile"),
+                allOf(withId(R.id.makeBookingButton), withText("Book"),
                         childAtPosition(
                                 allOf(withId(R.id.constraintLayout),
                                         childAtPosition(
                                                 withId(android.R.id.content),
                                                 0)),
-                                3),
+                                4),
                         isDisplayed()));
         button.perform(click());
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.netID), withText("NetID: 1000000001"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView.check(matches(withText("NetID: 1000000001")));
 
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.fullName), withText("Name: mark redekopp"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView2.check(matches(withText("Name: mark redekopp")));
+        Thread.sleep(1000);
+        Assert.assertEquals(mBookingTestRule.getActivity().getTitle(), "VILLAGE CENTER");
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
