5420ef503249824981991d99a6cf162de5c27cc4
Yuxing Zhou <tomzhouyuxing@gmail.com>
2022-04-13 23:33:44 -0700

    added waitlist test

---
+++ app/src/androidTest/java/com/example/project2bookingsample/ui/login/BookingWaitlistTest.java
@@ -0,0 +1,256 @@
+package com.example.project2bookingsample.ui.login;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.project2bookingsample.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BookingWaitlistTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void bookingWaitlistTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(longClick());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("1000000001"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.username), withText("1000000001"),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText4.perform(pressImeActionButton());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("password"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.password), withText("password"),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText6.perform(pressImeActionButton());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.makeBookingButton), withText("Book"),
+                        childAtPosition(
+                                allOf(withId(R.id.constraintLayout),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                4),
+                        isDisplayed()));
+        button.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.tmr), withText("TMR"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.main_linear),
+                                        5),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.time6), withText("11:00\n\nLeft:\n0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.main_linear),
+                                        2),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.book), withText("REMIND ME"),
+                        withParent(withParent(withId(R.id.main_linear))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.book), withText("Remind me"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.main_linear),
+                                        5),
+                                1),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(IsInstanceOf.<View>instanceOf(android.widget.TextView.class), withText("Booking Successful!"),
+                        withParent(allOf(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Booking Successful!")));
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(android.R.id.button2), withText("Close"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        materialButton4.perform(scrollTo(), click());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.back), withText("Back"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.main_linear),
+                                        5),
+                                3),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.button3), withText("View Booking Summary"),
+                        childAtPosition(
+                                allOf(withId(R.id.constraintLayout),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                8),
+                        isDisplayed()));
+        button3.perform(click());
+
+        DataInteraction materialTextView = onData(anything())
+                .inAdapterView(allOf(withId(R.id.futureBookingList),
+                        childAtPosition(
+                                withClassName(is("android.widget.RelativeLayout")),
+                                4)))
+                .atPosition(0);
+        materialTextView.perform(click());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(android.R.id.button1), withText("Yes"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton6.perform(scrollTo(), click());
+
+        ViewInteraction materialButton7 = onView(
+                allOf(withId(android.R.id.button2), withText("Close"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        materialButton7.perform(scrollTo(), click());
+
+        ViewInteraction materialButton8 = onView(
+                allOf(withId(R.id.back), withText("Back to Booking page"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialButton8.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
